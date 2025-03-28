ce529c1761d4590a1a976f7431aff847ce6a8351
Sebastian <salobo@uc.cl>
2025-03-28 17:12:16 -0300

    add(all-files): The function to convert from csv to html table is incorporated. The library for documenting is added: jsdoc. The use of node is standardized.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const { processCSVFile} = require('./helper.js');
-const {columnsToRows, rowsToColumns, columnDelete} = require("./functions");
-
-const filePath = "./dataExample/dataExample.csv";
-// example
-processCSVFile(columnsToRows, filePath, './dataExample/output-colsToRows.csv');
-processCSVFile(rowsToColumns, filePath, './dataExample/output-rowsToCols.csv');
-processCSVFile(columnDelete(1), filePath, './dataExample/output-columnDelete.csv');
